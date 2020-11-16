21812bdac854f8c2ba5aeec45f68934c6dad27a7
smarthkb98@gmail.com <smarthkb98@gmail.com>
2020-11-16 20:29:47 +0900

    webcam.py first run

--- webcam.py
+++ webcam.py
@@ -4,6 +4,49 @@ import os
 from sys import platform
 import argparse
 
+# prev_poseKeypoints =
+
+def distance(prev, curr):
+    return (prev[0] - curr[0]) ** 2 + (prev[1] - curr[1]) ** 2
+
+
+def metric(pair_poseKeypoints):
+    assert len(pair_poseKeypoints) == 2
+    prev_poseKeypoints =  pair_poseKeypoints[0]
+    poseKeypoints = pair_poseKeypoints[1]
+
+    print('insie metric', type(prev_poseKeypoints))
+
+    if len(prev_poseKeypoints) == 0 or len(poseKeypoints) == 0:
+        return -1000
+    print(len(prev_poseKeypoints), len(poseKeypoints))
+    prev_nose, nose = prev_poseKeypoints[0], poseKeypoints[0]
+    # print(prev_nose, nose)
+    prev_neck, neck = prev_poseKeypoints[1], poseKeypoints[1]
+    prev_right_shoulder, right_shoulder = prev_poseKeypoints[2], poseKeypoints[2]
+    prev_left_shoulder, left_shoulder = prev_poseKeypoints[5], poseKeypoints[5]
+    prev_right_eye, right_eye = prev_poseKeypoints[15], poseKeypoints[15]
+    prev_left_eye, left_eye = prev_poseKeypoints[16], poseKeypoints[16]
+    prev_right_ear, right_ear = prev_poseKeypoints[17], poseKeypoints[17]
+    prev_left_ear, left_ear = prev_poseKeypoints[18], poseKeypoints[18]
+
+    distance_nose = distance(prev_nose, nose)
+    distance_neck = distance(prev_neck, neck)
+    distance_right_shoulder = distance(prev_right_shoulder, right_shoulder)
+    distance_left_shoulder = distance(prev_left_shoulder, left_shoulder)
+    distance_right_eye = distance(prev_right_eye, right_eye)
+    distance_left_eye = distance(prev_left_eye, left_eye)
+    distance_right_ear = distance(prev_right_ear, right_ear)
+    distance_left_ear = distance(prev_left_ear, left_ear)
+
+    if distance_nose > 5000:
+        return True
+    elif distance_neck > 4000:
+        return True
+    else:
+        return False
+
+
 try:
     # Import Openpose (Windows/Ubuntu/OSX)
     dir_path = os.path.dirname(os.path.realpath(__file__))
@@ -39,7 +82,7 @@ try:
     # params["hand"] = True
     # params["hand_detector"] = 2
     params["body"] = 1
-    params["face"] = True
+    # params["face"] = True
     # params["disable_blending"] = True
     params["fps_max"] = 12
 
@@ -70,21 +113,43 @@ try:
     datum = op.Datum()
     datum.handRectangles = handRectangles
     cam = cv2.VideoCapture(0)  # modify here for camera number
-    cam.set(3, 1280)
-    cam.set(4, 720)
+    # cam.set(3, 1280)
+    # cam.set(4, 720)
+    pair_poseKeypoints = [[], []]
     while (cv2.waitKey(1) != 27):
         # Get camera frame
+        print('Another Frame')
         ret, frame = cam.read()
         # print(frame.shape[:2])
         datum.cvInputData = frame
         opWrapper.emplaceAndPop([datum])
+        print('Here?')
         frame = datum.cvOutputData
-        print(datum.poseKeypoints)
+        print(datum.poseKeypoints, datum.poseKeypoints.shape)
+        # print(datum.poseKeypoints.shape == ())
+        if datum.poseKeypoints.shape == ():
+            cv2.imshow("Openpose 1.4.0 Webcam", frame)  # datum.cvOutputData
+            continue
+        # if
+        print('new pose', len(datum.poseKeypoints[0]))
+        del pair_poseKeypoints[0]
+        print('Popped pair')
+        pair_poseKeypoints.append(datum.poseKeypoints[0])
+        print('Appended new pose')
+        # print(len(pair_poseKeypoints))
+        # if pair_poseKeypoints[0] != None:
+        #     print(pair_poseKeypoints[0][0])
+        # print(datum.poseKeypoints)
+        # print(pair_poseKeypoints)
+        moved = metric(pair_poseKeypoints)
+        print(moved)
+
         # left_hand = datum.handKeypoints[0]
         # right_hand = datum.handKeypoints[1]
         # print(left_hand)
-        # cv2.rectangle(frame, (100, 150), (328, 328), (0, 255, 0), 1, 1)
-        # cv2.rectangle(frame, (0, 0), (328, 328), (0, 255, 0), 1, 1)
+        if (moved):
+            # cv2.rectangle(frame, (100, 150), (328, 328), (0, 255, 0), 1, 1)
+            cv2.rectangle(frame, (0, 0), (640, 480), (0, 0, 255), 20)
         cv2.imshow("Openpose 1.4.0 Webcam", frame)  # datum.cvOutputData
     # Always clean up
     cam.release()
